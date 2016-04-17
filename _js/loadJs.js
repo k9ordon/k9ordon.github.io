301b8875d4f6a312c7816f5fc30747affdbbea8e
kschreiber <klemens.schreiber@karriere.at>
2016-04-17 14:55:56 +0200

    loadJs with callback 🍄

--- _js/loadJs.js
+++ _js/loadJs.js
@@ -1,6 +1,12 @@
-function loadJs(src) {
-    var resource = document.createElement('script');
-    resource.src = src;
-    var script = document.getElementsByTagName('script')[0];
-    script.parentNode.insertBefore(resource, script);
+function loadJs(src, callback) {
+    var $resource = document.createElement('script');
+    $resource.src = src;
+    $resource.type = "text/javascript";
+    $resource.async = true;
+    $resource.addEventListener('load', function() {
+        if(callback) callback();
+    });
+
+    var $script = document.getElementsByTagName('script')[0];
+    $script.parentNode.insertBefore($resource, $script);
 }
